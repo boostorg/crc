7a02e889255e7b1f966846b78a6475d56e110b9b
Peter Dimov <pdimov@gmail.com>
2024-04-14 03:07:48 +0300

    Avoid triggering the `max` macro from <windows.h>. Fixes #25.

--- include/boost/crc.hpp
+++ include/boost/crc.hpp
@@ -616,7 +616,7 @@ namespace detail
         }
 
         // Clear overflowed bits
-        remainder &= std::numeric_limits<Register>::max() >> (std::numeric_limits<Register>::digits - register_length);
+        remainder &= (std::numeric_limits<Register>::max)() >> (std::numeric_limits<Register>::digits - register_length);
     }
 
     /** \brief  Update a CRC remainder by a single bit, assuming a non-augmented
